99148b3a00359846f8a96cdc741afa29038c639e
tdgunes <tdgunes@gmail.com>
2015-06-29 02:04:27 +0300

    Fix listing for preserving the order of areas

--- policy/views.py
+++ policy/views.py
@@ -109,8 +109,8 @@ class GetAllPolicies(APIView):
         my_policies = self.set_up_policies(server_name, server_url, serializer.data)
         policies.insert(0, my_policies)
 
-        for super_policy in super_policies:
-            policies.insert(0, super_policy)
+        policies = super_policies + policies
+
 
         return Response(policies, status=status.HTTP_200_OK)
 
